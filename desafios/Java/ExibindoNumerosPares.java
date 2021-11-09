b7267845e9e3ffe0657e17c70423e48a09194d03
Luis Carlos <78564001+didifive@users.noreply.github.com>
2021-11-09 14:08:13 -0300

    Update ExibindoNumerosPares.java

--- desafios/Java/ExibindoNumerosPares.java
+++ desafios/Java/ExibindoNumerosPares.java
@@ -16,7 +16,7 @@ Exiba todos os números pares até o valor de entrada, sendo um em cada linha.
 
 -----------------------------------------
 | Exemplo de Entrada | Exemplo de Saída |
-| 6	                 | 2                |
+| 6	             | 2                |
 |                    | 4                |
 |                    | 6                |
 -----------------------------------------
