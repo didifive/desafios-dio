01204094f74c7561ac6fd51bec3f706b71f870f2
Luis Carlos <78564001+didifive@users.noreply.github.com>
2021-11-09 20:30:43 -0300

    Create SeisNumerosImpares.java
    Create SeisNumerosImpares.java with description of the challenge "3/5 - Folha de Pagamento" and respective solution.

---
+++ desafios/Java/Desafios matemáticos em Java/SeisNumerosImpares.java
@@ -0,0 +1,47 @@
+/*
+====================================================
+Solucionando desafios matemáticos em Java
+====================================================
+2/3 - Seis Números Ímpares
+====================================================
+
+Leia um valor inteiro X. Em seguida apresente os 6 valores ímpares consecutivos a partir de X, um valor por linha, inclusive o X se for o caso.
+
+Entrada
+A entrada será um valor inteiro positivo.
+
+Saída
+A saída será uma sequência de seis números ímpares.
+
+--------------------------------------------
+| Exemplo de Entrada | Exemplo de Saída    |
+--------------------------------------------
+| 8                  | 9                   |
+|                    | 11                  |
+|                    | 13                  |
+|                    | 15                  |
+|                    | 17                  |
+|                    | 19                  |
+--------------------------------------------
+
+SOLUÇÃO ABAIXO: */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SeisNumerosImpares {
+	
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int x = leitor.nextInt();
+        int cont = 0;
+
+        while (cont < 12 ) {
+        	if ( x % 2  == 1) {
+        		System.out.println(x);
+        	}
+    		cont++;
+    		x++;
+        }
+    }
+}
