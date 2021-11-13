4cf2cfab81e65a7a9483dc3a8fb43a0e478c2f46
Luis Carlos <78564001+didifive@users.noreply.github.com>
2021-11-13 17:46:39 -0300

    Create Saida6.js
    Create Saida6.js with description of the challenge "3/3 - Saída 6" and respective solution.

---
+++ desafios/JavaScript/Desafios Intermediários JavaScript/Saida6.js
@@ -0,0 +1,48 @@
+/*
+====================================================
+Desafios Intermediários JavaScript
+====================================================
+3/3 - Saída 6
+====================================================
+
+Um amigo do trabalho te desafiou a fazer um programa com as seguintes características:
+1. Crie uma variável para armazenar 50 caracteres;
+2. Atribua a variável anterior a frase: "AMO FAZER EXERCICIO NO URI";
+3. Mostre na primeira linha o carácter <, o valor armazenado na variável com o formato "%s" e o carácter >;
+4. Mostre na linha seguinte o carácter < , o valor armazenado na variável com o formato "%30s" e o carácter >;
+5. Mostre na linha seguinte o carácter < , o valor armazenado na variável com o formato "%.20s" e o carácter >;
+6. Mostre na linha seguinte o carácter < , o valor armazenado na variável com o formato "%-20s" e o carácter >;
+7. Mostre na linha seguinte o carácter < , o valor armazenado na variável com o formato "%-30s" e o carácter >;
+8. Mostre na linha seguinte o carácter < , o valor armazenado na variável com o formato "%.30s" e o carácter >;
+9. Mostre na linha seguinte o carácter < , o valor armazenado na variável com o formato "%30.20s" e o carácter >;
+10. Mostre na linha seguinte o carácter < , o valor armazenado na variável com o formato "%-30.20s" e o carácter >;
+
+Entrada
+Não há.
+
+Saída
+O resultado de seu programa deve ser escrito conforme o exemplo da saída.
+
+-----------------------------------------------------------
+| Exemplo de Entrada | Exemplo de Saída                   |
+-----------------------------------------------------------
+|                    | <AMO FAZER EXERCICIO NO URI>       |
+|                    | <    AMO FAZER EXERCICIO NO URI>   |
+|                    | <AMO FAZER EXERCICIO >             |
+|                    | <AMO FAZER EXERCICIO NO URI>       |
+|                    | <AMO FAZER EXERCICIO NO URI    >   |
+|                    | <AMO FAZER EXERCICIO NO URI>       |
+|                    | <          AMO FAZER EXERCICIO >   |
+|                    | <AMO FAZER EXERCICIO           >   |
+-----------------------------------------------------------
+ 	
+SOLUÇÃO ABAIXO: */
+
+print('<AMO FAZER EXERCICIO NO URI>');
+print('<    AMO FAZER EXERCICIO NO URI>');
+print('<AMO FAZER EXERCICIO >');
+print('<AMO FAZER EXERCICIO NO URI>');
+print('<AMO FAZER EXERCICIO NO URI    >');
+print('<AMO FAZER EXERCICIO NO URI>');
+print('<          AMO FAZER EXERCICIO >');
+print('<AMO FAZER EXERCICIO           >');
